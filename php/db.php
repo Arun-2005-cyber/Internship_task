76fc0999918ed05a6225febb49d2c8e03d2bcc62
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-12-07 14:08:35 +0530

    new

--- php/db.php
+++ php/db.php
@@ -4,17 +4,17 @@ $user = getenv("MYSQLUSER");
 $pass = getenv("MYSQLPASSWORD");
 $db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
-$mongoUrl = getenv("MONGO_URL");
+// $mongoUrl = getenv("MONGO_URL");
 
 
-try {
-    $mongoClient = new MongoDB\Client($mongoUrl);
-} catch (Exception $e) {
-    die(json_encode([
-        'success' => false,
-        'message' => 'MongoDB connection failed: ' . $e->getMessage()
-    ]));
-}
+// try {
+//     $mongoClient = new MongoDB\Client($mongoUrl);
+// } catch (Exception $e) {
+//     die(json_encode([
+//         'success' => false,
+//         'message' => 'MongoDB connection failed: ' . $e->getMessage()
+//     ]));
+// }
 
 $mysqli = new mysqli($host, $user, $pass, $db, $port);
 
